875ec8c32bacb1bbf427d13158c8e5fcfb3553bb
MessyPrincy <alexan2004andw@gmail.com>
2024-07-26 22:52:43 -0400

    Rewrote the structure in other to support slash commands

--- bot.py
+++ bot.py
@@ -1,53 +1,81 @@
 import discord
 from discord.ext import commands, tasks
-import os
-import random
-import platform
+from discord.ext.commands import Context
 from dotenv import load_dotenv
+import os
 import logging
-
-CYAN = '\033[96m'
-RED = '\033[91m'
-RESET = '\033[0m'
-
-class CustomFormatter(logging.Formatter):
-    """Custom logging formatter to add colors based on log level."""
-    
-    def format(self, record):
-        if record.levelno == logging.INFO:
-            prefix = CYAN
-        elif record.levelno >= logging.ERROR:
-            prefix = RED
-        else:
-            prefix = RESET
-        original_format = super().format(record)
-        return f"{prefix}{original_format}{RESET}"
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger('Upsilon')
-logger.propagate = False
-handler = logging.StreamHandler()
-handler.setFormatter(CustomFormatter('%(levelname)s - %(message)s'))
-logger.addHandler(handler)
+import random
 
 intents = discord.Intents.default()
 intents.message_content = True
 
+
+class LoggingFormatter(logging.Formatter):
+    black = "\x1b[30m"
+    red = "\x1b[31m"
+    green = "\x1b[32m"
+    yellow = "\x1b[33m"
+    blue = "\x1b[34m"
+    gray = "\x1b[38m"
+    reset = "\x1b[0m"
+    bold = "\x1b[1m"
+
+    COLORS = {
+        logging.DEBUG: gray + bold,
+        logging.INFO: blue + bold,
+        logging.WARNING: yellow + bold,
+        logging.ERROR: red,
+        logging.CRITICAL: red + bold,
+    }
+
+    def format(self, record):
+        log_color = self.COLORS[record.levelno]
+        formatting = "(black){asctime}(reset) (levelcolor){levelname:<8}(reset) (green){name}(reset) {message}"
+        formatting = formatting.replace("(black)", self.black + self.bold)
+        formatting = formatting.replace("(reset)", self.reset)
+        formatting = formatting.replace("(levelcolor)", log_color)
+        formatting = formatting.replace("(green)", self.green + self.bold)
+        formatter = logging.Formatter(formatting, "%Y-%m-%d %H:%M:%S", style="{")
+        return formatter.format(record)
+
+
+logger = logging.getLogger("Upsilon")
+logger.setLevel(logging.INFO)
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(LoggingFormatter())
+
+file_handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+file_handler_formatter = logging.Formatter(
+    "[{asctime}] [{levelname:<8}] {name}: {message}", "%Y-%m-%d %H:%M:%S", style="{"
+)
+file_handler.setFormatter(file_handler_formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
 class MyBot(commands.Bot):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self) -> None:
+        super().__init__(
+            command_prefix='.',
+            intents=intents,
+            help_command=None
+        )
         self.logger = logger
 
-    async def load_cogs(self):
+    async def load_cogs(self) -> None:
         for file in os.listdir(f"{os.path.realpath(os.path.dirname(__file__))}/cogs"):
             if file.endswith(".py"):
                 extension = file[:-3]
                 try:
                     await self.load_extension(f"cogs.{extension}")
-                    self.logger.info(f"Loaded extension '{extension}'")
+                    self.logger.info(f"Loaded cog '{extension}'")
                 except Exception as e:
                     exception = f"{type(e).__name__}: {e}"
-                    self.logger.error(f"Failed to load extension {extension}\n{exception}")
+                    self.logger.error(
+                        f"Failed to load cog {extension}\n{exception}"
+                    )
 
     async def reload_cogs(self):
         for file in os.listdir(f"{os.path.realpath(os.path.dirname(__file__))}/cogs"):
@@ -67,26 +95,87 @@ class MyBot(commands.Bot):
                     exception = f"{type(e).__name__}: {e}"
                     self.logger.error(f"Failed to reload extension {extension}\n{exception}")
 
-
     @tasks.loop(minutes=1.0)
-    async def status_task(self):
-        statuses = ["with the API", "with the code", "with the database", "with the logs"]
-        await self.change_presence(activity=discord.Game(random.choice(statuses)))
+    async def status(self) -> None:
+        statuses = ["with discord.py", "with you humans", "to win", "on a linux computer"]
+        await self.change_presence(activity=discord.Game(name=random.choice(statuses)))
 
-    @status_task.before_loop
-    async def before_status_task(self):
+    @status.before_loop
+    async def before_status(self) -> None:
+        await self.load_cogs()
         await self.wait_until_ready()
 
-    async def setup_hook(self):
-        self.logger.info(f"Logged in as {self.user.name}")
-        self.logger.info(f"discord.py API version: {discord.__version__}")
-        self.logger.info(f"Python version: {platform.python_version()}")
-        self.logger.info(f"Running on: {platform.system()} {platform.release()} ({os.name})")
-        await self.load_cogs()
-        self.status_task.start()
+    async def on_ready(self):
+        self.logger.info(f'Logged in as {self.user}')
+        self.status.start()
+        await self.tree.sync()
 
-bot = MyBot(command_prefix='.', intents=intents)
+    async def on_command_completion(self, context: Context) -> None:
+        full_command_name = context.command.qualified_name
+        split = full_command_name.split(" ")
+        executed_command = str(split[0])
+        if context.guild is not None:
+            self.logger.info(
+                f"Executed {executed_command} command in {context.guild.name} (ID: {context.guild.id}) by {context.author} (ID: {context.author.id})"
+            )
+        else:
+            self.logger.info(
+                f"Executed {executed_command} command by {context.author} (ID: {context.author.id}) in DMs"
+            )
+
+    async def on_command_error(self, context: Context, error) -> None:
+        if isinstance(error, commands.CommandOnCooldown):
+            minutes, seconds = divmod(error.retry_after, 60)
+            hours, minutes = divmod(minutes, 60)
+            hours = hours % 24
+            embed = discord.Embed(
+                description=f"**Please slow down** - You can use this command again in {f'{round(hours)} hours' if round(hours) > 0 else ''} {f'{round(minutes)} minutes' if round(minutes) > 0 else ''} {f'{round(seconds)} seconds' if round(seconds) > 0 else ''}.",
+                color=0xE02B2B,
+            )
+            await context.send(embed=embed)
+        elif isinstance(error, commands.NotOwner):
+            embed = discord.Embed(
+                description="You are not the owner of the bot!", color=0xE02B2B
+            )
+            await context.send(embed=embed)
+            if context.guild:
+                self.logger.warning(
+                    f"{context.author} (ID: {context.author.id}) tried to execute an owner only command in the guild {context.guild.name} (ID: {context.guild.id}), but the user is not an owner of the bot."
+                )
+            else:
+                self.logger.warning(
+                    f"{context.author} (ID: {context.author.id}) tried to execute an owner only command in the bot's "
+                    f"DMs, but the user is not an owner of the bot."
+                )
+        elif isinstance(error, commands.MissingPermissions):
+            embed = discord.Embed(
+                description="You are missing the permission(s) `"
+                            + ", ".join(error.missing_permissions)
+                            + "` to execute this command!",
+                color=0xE02B2B,
+            )
+            await context.send(embed=embed)
+        elif isinstance(error, commands.BotMissingPermissions):
+            embed = discord.Embed(
+                description="I am missing the permission(s) `"
+                            + ", ".join(error.missing_permissions)
+                            + "` to fully perform this command!",
+                color=0xE02B2B,
+            )
+            await context.send(embed=embed)
+        elif isinstance(error, commands.MissingRequiredArgument):
+            embed = discord.Embed(
+                title="Error!",
+                description=str(error).capitalize(),
+                color=0xE02B2B,
+            )
+            await context.send(embed=embed)
+        else:
+            raise error
+
 
 load_dotenv()
-
+bot = MyBot()
 bot.run(os.getenv('DISCORD_TOKEN'))
+
+# Basically copied from https://github.com/kkrypt0nn/Python-Discord-Bot-Template?tab=readme-ov-file by kkrypt0nn
--- cogs/Misc.py
+++ cogs/Misc.py
@@ -1,17 +1,29 @@
+import os
+from dotenv import load_dotenv
+import discord
+from discord import app_commands
 from discord.ext import commands
+from discord.ext.commands import Context
 
-class Misc(commands.Cog):
-    def __init__(self, bot):
+load_dotenv()
+GUILD_ID = int(os.getenv("GUILD_ID"))
+
+
+class Misc(commands.Cog, name="misc"):
+    def __init__(self, bot) -> None:
         self.bot = bot
 
-    @commands.command(aliases=['git', 'source'])
-    async def github(self, ctx):
-        await ctx.send('https://github.com/MessyPrincy/Upsilon')
+    @commands.hybrid_command(name="ping", description="Check the bot's latency.")
+    @app_commands.guilds(discord.Object(id=GUILD_ID))
+    async def ping(self, ctx: Context) -> None:
+        await ctx.send(f"Pong! {round(self.bot.latency * 1000)}ms")
 
-    @commands.command()
-    async def ping(self, ctx):
-        await ctx.send(f'Pong! {round(self.bot.latency * 1000)}ms')
+    @commands.hybrid_command(name="git", description="Sends the GitHub repository link.")
+    @app_commands.guilds(discord.Object(id=GUILD_ID))
+    async def git(self, ctx: Context) -> None:
+        await ctx.send("https://github.com/MessyPrincy/Upsilon")
 
 
-async def setup(bot):
+async def setup(bot) -> None:
     await bot.add_cog(Misc(bot))
+    await bot.tree.sync(guild=discord.Object(id=GUILD_ID))
